47d13bf79d7084ac0ca7d0457d9feff9e491df83
TaylorFullstack <heather.taylor.dev@gmail.com>
2023-09-12 13:23:35 +0200

    update snap counts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent implements OnInit {
       description: "Le plus câlin",
       imageUrl: "https://cdn.pixabay.com/photo/2020/11/26/15/06/cat-5779248_1280.jpg",
       createdDate: new Date(),
-      snaps: 0,
+      snaps: 1,
       location: "Port Marianne"
       },
       {
@@ -25,7 +25,7 @@ export class AppComponent implements OnInit {
         description: "La plus bruyante des ronronneuses",
         imageUrl: "https://cdn.pixabay.com/photo/2020/02/18/15/29/cat-4859779_1280.jpg",
         createdDate: new Date(),
-        snaps: 0,
+        snaps: 5,
         location: "Beaux Arts"
       },
       {
@@ -33,7 +33,7 @@ export class AppComponent implements OnInit {
         description: "Le plus calme",
         imageUrl: "https://cdn.pixabay.com/photo/2018/10/08/19/51/cat-3733316_1280.jpg",
         createdDate: new Date(),
-        snaps: 0,
+        snaps: 10,
       }
     ]
   }
