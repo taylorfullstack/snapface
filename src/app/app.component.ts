42b073724e2f8aac686a769e24d9928f6e852620
TaylorFullstack <heather.taylor.dev@gmail.com>
2023-09-12 16:11:31 +0200

    remove code that was migrated to list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
@@ -8,33 +7,5 @@ import { FaceSnap } from './models/face-snap.model';
 })
 export class AppComponent implements OnInit {
 
-  faceSnaps!: FaceSnap[];
-
-  ngOnInit() {
-    this.faceSnaps = [
-      {
-      title: "Onix",
-      description: "Le plus câlin",
-      imageUrl: "https://cdn.pixabay.com/photo/2020/11/26/15/06/cat-5779248_1280.jpg",
-      createdDate: new Date(),
-      snaps: 1,
-      location: "Port Marianne"
-      },
-      {
-        title: "Saphir",
-        description: "La plus bruyante des ronronneuses",
-        imageUrl: "https://cdn.pixabay.com/photo/2020/02/18/15/29/cat-4859779_1280.jpg",
-        createdDate: new Date(),
-        snaps: 5,
-        location: "Beaux Arts"
-      },
-      {
-        title: "Citrine",
-        description: "Le plus calme",
-        imageUrl: "https://cdn.pixabay.com/photo/2018/10/08/19/51/cat-3733316_1280.jpg",
-        createdDate: new Date(),
-        snaps: 10,
-      }
-    ]
-  }
+  ngOnInit() {}
 }
