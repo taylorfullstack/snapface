b63e0814c4a035e018aaece00444760d6f5eb681
TaylorFullstack <heather.taylor.dev@gmail.com>
2023-09-12 12:07:46 +0200

    add three snaps on app initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'snapface';
+export class AppComponent implements OnInit {
+  
+  mySnap!: FaceSnap;
+  myOtherSnap!: FaceSnap;
+  myLastSnap!: FaceSnap;
+
+  ngOnInit() {
+    this.mySnap = {
+      title: "Onix",
+      description: "Le plus câlin",
+      imageUrl: "https://cdn.pixabay.com/photo/2020/11/26/15/06/cat-5779248_1280.jpg",
+      createdDate: new Date(),
+      snaps: 0,
+      location: "Port Marianne"
+    };
+    this.myOtherSnap = {
+      title: "Saphir",
+      description: "La plus bruyante des ronronneuses",
+      imageUrl: "https://cdn.pixabay.com/photo/2020/02/18/15/29/cat-4859779_1280.jpg",
+      createdDate: new Date(),
+      snaps: 0,
+      location: "Beaux Arts"
+    };
+    this.myLastSnap = {
+      title: "Citrine",
+      description: "Le plus calme",
+      imageUrl: "https://cdn.pixabay.com/photo/2018/10/08/19/51/cat-3733316_1280.jpg",
+      createdDate: new Date(),
+      snaps: 0,
+    };
+}
 }
