d6831c0ef038eda710ee82a3ed3b50dbc3e7da4d
TaylorFullstack <heather.taylor.dev@gmail.com>
2023-09-12 12:14:21 +0200

    replace individual faceSnaps with an array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,34 +7,34 @@ import { FaceSnap } from './models/face-snap.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
+
+  faceSnaps!: FaceSnap[];
 
   ngOnInit() {
-    this.mySnap = {
+    this.faceSnaps = [
+      {
       title: "Onix",
       description: "Le plus câlin",
       imageUrl: "https://cdn.pixabay.com/photo/2020/11/26/15/06/cat-5779248_1280.jpg",
       createdDate: new Date(),
       snaps: 0,
       location: "Port Marianne"
-    };
-    this.myOtherSnap = {
-      title: "Saphir",
-      description: "La plus bruyante des ronronneuses",
-      imageUrl: "https://cdn.pixabay.com/photo/2020/02/18/15/29/cat-4859779_1280.jpg",
-      createdDate: new Date(),
-      snaps: 0,
-      location: "Beaux Arts"
-    };
-    this.myLastSnap = {
-      title: "Citrine",
-      description: "Le plus calme",
-      imageUrl: "https://cdn.pixabay.com/photo/2018/10/08/19/51/cat-3733316_1280.jpg",
-      createdDate: new Date(),
-      snaps: 0,
-    };
-}
+      },
+      {
+        title: "Saphir",
+        description: "La plus bruyante des ronronneuses",
+        imageUrl: "https://cdn.pixabay.com/photo/2020/02/18/15/29/cat-4859779_1280.jpg",
+        createdDate: new Date(),
+        snaps: 0,
+        location: "Beaux Arts"
+      },
+      {
+        title: "Citrine",
+        description: "Le plus calme",
+        imageUrl: "https://cdn.pixabay.com/photo/2018/10/08/19/51/cat-3733316_1280.jpg",
+        createdDate: new Date(),
+        snaps: 0,
+      }
+    ]
+  }
 }
