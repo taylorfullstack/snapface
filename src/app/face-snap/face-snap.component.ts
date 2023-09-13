52ccee21b7aa5df6263efed89fe94694ae7758e9
TaylorFullstack <heather.taylor.dev@gmail.com>
2023-09-13 15:17:04 +0200

    refactor face snap component, method moved to single face snap

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapsService } from '../services/face-snaps.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-face-snap',
@@ -11,19 +12,13 @@ export class FaceSnapComponent implements OnInit{
   @Input() faceSnap!: FaceSnap;
   buttonText!: string;
 
-  constructor(private faceSnapsService: FaceSnapsService) {}
+  constructor(private faceSnapsService: FaceSnapsService, private router: Router) {}
 
   ngOnInit() {
-      this.buttonText = "Oh Snap!"
-    }
+    this.faceSnap = this.faceSnapsService.getFaceSnapById(this.faceSnap.id);
+  }
 
-  onSnap(){
-    if(this.buttonText === "Oh Snap!"){
-      this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'snap');
-      this.buttonText = "Oops, unSnap!"
-    } else {
-      this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'unsnap');
-      this.buttonText = "Oh Snap!";
-    }
+  onViewFaceSnap(){
+    this.router.navigateByUrl(`facesnaps/${this.faceSnap.id}`);
   }
 }
