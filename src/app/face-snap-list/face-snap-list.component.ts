ffd815ef351b281e898ccdb043483a8dd85c3da3
TaylorFullstack <heather.taylor.dev@gmail.com>
2023-09-13 10:02:29 +0200

    refactor the face snap list and face snap components to use the methods in the service

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
+import { FaceSnapsService } from '../services/face-snaps.service';
 
 @Component({
   selector: 'app-face-snap-list',
@@ -10,32 +11,10 @@ export class FaceSnapListComponent implements OnInit{
 
   faceSnaps!: FaceSnap[];
 
-  ngOnInit() {
-    this.faceSnaps = [
-      {
-      title: "Onix",
-      description: "Le plus câlin",
-      imageUrl: "https://cdn.pixabay.com/photo/2020/11/26/15/06/cat-5779248_1280.jpg",
-      createdDate: new Date(),
-      snaps: 1,
-      location: "Port Marianne"
-      },
-      {
-        title: "Saphir",
-        description: "La plus bruyante des ronronneuses",
-        imageUrl: "https://cdn.pixabay.com/photo/2020/02/18/15/29/cat-4859779_1280.jpg",
-        createdDate: new Date(),
-        snaps: 5,
-        location: "Beaux Arts"
-      },
-      {
-        title: "Citrine",
-        description: "Le plus calme",
-        imageUrl: "https://cdn.pixabay.com/photo/2018/10/08/19/51/cat-3733316_1280.jpg",
-        createdDate: new Date(),
-        snaps: 10,
-      }
-    ]
+  constructor(private faceSnapsService: FaceSnapsService){}
+
+  ngOnInit(): void {
+    this.faceSnaps = this.faceSnapsService.getAllFaceSnaps();
   }
 
 }
