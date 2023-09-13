96d1990c7b34162658657b0186d6b37958c815ac
TaylorFullstack <heather.taylor.dev@gmail.com>
2023-09-13 13:26:31 +0200

    add onContinue() to programmatically change routes

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class LandingPageComponent {
 
+  constructor(private router: Router) { }
+
+  onContinue() {
+    this.router.navigateByUrl('facesnaps');
+  }
 }
